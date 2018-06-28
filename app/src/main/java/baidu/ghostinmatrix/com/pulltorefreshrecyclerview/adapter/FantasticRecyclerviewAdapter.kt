ced808ec5dd78e046e9072ab89defb0bba3d2473
单劼 <shanjie1@sf-express.com>
2018-06-28 16:30:22 +0800

    FantasticAdapter now is able to support MultiTypeLayout.FantasticActivity will show you how to use it. add api getDataCount() and appendAData()

--- app/src/main/java/baidu/ghostinmatrix/com/pulltorefreshrecyclerview/adapter/FantasticRecyclerviewAdapter.kt
+++ app/src/main/java/baidu/ghostinmatrix/com/pulltorefreshrecyclerview/adapter/FantasticRecyclerviewAdapter.kt
@@ -46,12 +46,12 @@ abstract class FantasticRecyclerviewAdapter<E : Any>(private val context: Contex
         datas?.let {
             mData.addAll(datas)
         }
-        
     }
     
     /**
      * choose on of a @AnimType as each Data Item anim when first attached to Window
      * @see AnimType
+     * 选择一个itemView的入场动画
      */
     fun anim(@AnimType animType: Int): FantasticRecyclerviewAdapter<E> {
         selectedAnim = animType
@@ -59,7 +59,7 @@ abstract class FantasticRecyclerviewAdapter<E : Any>(private val context: Contex
     }
     
     /**
-     * customize headers as needed
+     * 自定义headers的resources
      */
     fun headers(headers: ArrayList<Int>): FantasticRecyclerviewAdapter<E> {
         mHeader.addAll(headers)
@@ -67,7 +67,7 @@ abstract class FantasticRecyclerviewAdapter<E : Any>(private val context: Contex
     }
     
     /**
-     * customize footers as needed
+     * 自定义footer的Resource
      */
     fun footers(footers: ArrayList<Int>): FantasticRecyclerviewAdapter<E> {
         mFooter.addAll(footers)
@@ -77,7 +77,9 @@ abstract class FantasticRecyclerviewAdapter<E : Any>(private val context: Contex
     
     /**
      * choose if the EmptyView will cover the headers and footers or not
-     * true is not cover
+     * true if not cover
+     * 可选择，空页面盖住header和footer，还是空页面在header和footer之间。
+     * true 为之间。
      */
     fun stickHeaderFooter(isStick: Boolean): FantasticRecyclerviewAdapter<E> {
         stickHeaderFooter = isStick
@@ -85,7 +87,8 @@ abstract class FantasticRecyclerviewAdapter<E : Any>(private val context: Contex
     }
     
     /**
-     * customize emptyView when adapter as no data
+     * customize emptyView when adapter has no data
+     * 自定义空页面，如果adapter内没有数据时，会自动展示
      */
     fun emptyView(emptyViewId: Int): FantasticRecyclerviewAdapter<E> {
         emptyLayoutId = emptyViewId
@@ -145,13 +148,22 @@ abstract class FantasticRecyclerviewAdapter<E : Any>(private val context: Contex
         }
     }
     
-    fun refreshData(group: ArrayList<E>) {
+    /**
+     * usually used when pull down to refresh
+     * 一般用于下拉刷新，清空[mData]重新注入
+     * [mData]是Adapter内部容器，data所在的外部列表不会被持有
+     */
+    fun refreshData(group: List<E>) {
         mData.clear()
         mData.addAll(group)
         firstItemAnimPosition = 0
         notifyDataSetChanged()
     }
     
+    /**
+     * append a list of data,usually used when pull up to load
+     * 一般用于分页加载，追加即可。
+     */
     fun appendData(append: List<E>?) {
         append?.let {
             mData.addAll(append)
@@ -159,15 +171,16 @@ abstract class FantasticRecyclerviewAdapter<E : Any>(private val context: Contex
         }
     }
     
-//    override fun getItemId(position: Int): Long {
-//        return getItemIdFromData(mData[position - mHeader.size])
-//    }
-//
-//    fun getItemIdFromData(data: E): Long {
-//        return RecyclerView.NO_ID
-//    }
+    /**
+     * append a single data element
+     * 新增单一数据
+     */
+    fun appendAData(element: E) {
+        mData.add(element)
+        notifyDataSetChanged()
+    }
     
-    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ComViewHolderKt {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ComViewHolderKt {
         return when {
             viewType == EMPTY -> ComViewHolderKt.getComViewHolder(context, emptyLayoutId, parent)
             viewType > HEADER && viewType <= (HEADER + mHeader.size) -> {
@@ -194,6 +207,18 @@ abstract class FantasticRecyclerviewAdapter<E : Any>(private val context: Contex
     
     abstract fun convert(viewHolderKt: ComViewHolderKt, data: E, type: Int, position: Int)
     
+    /**
+     * 得到真实数据的数量，不包括header/footer/empty等
+     */
+    public fun getDataCount(): Int {
+        return mData.size
+    }
+    
+    /**
+     * adapter内部使用判断各个item数量，并非外部数据的总量。一般在没有header、footer且不为空的情况下，二者是相同的
+     * 否则数量大于等于 getDataCount()
+     * @see getDataCount
+     */
     override fun getItemCount(): Int {
         var count = 0
         if (mData.isEmpty()) {
@@ -208,6 +233,10 @@ abstract class FantasticRecyclerviewAdapter<E : Any>(private val context: Contex
         return count
     }
     
+    
+    /**
+     * 删除单个item
+     */
     fun removeItem(holder: ComViewHolderKt) {
         val position = holder.layoutPosition
         if (mData.isNotEmpty() && position >= mHeader.size && position < mHeader.size + mData.size) {
@@ -219,6 +248,9 @@ abstract class FantasticRecyclerviewAdapter<E : Any>(private val context: Contex
         }
     }
     
+    /**
+     * 置顶单个Item
+     */
     fun topItem(holder: ComViewHolderKt) {
         val position = holder.layoutPosition
         if (mData.isNotEmpty() && position >= mHeader.size && position < mHeader.size + mData.size) {
@@ -229,7 +261,6 @@ abstract class FantasticRecyclerviewAdapter<E : Any>(private val context: Contex
     }
     
     override fun getItemViewType(position: Int): Int {
-        
         return if (mData.isEmpty()) {
             if (stickHeaderFooter) {
                 when {
@@ -251,6 +282,4 @@ abstract class FantasticRecyclerviewAdapter<E : Any>(private val context: Contex
             }
         }
     }
-    
-    
 }
